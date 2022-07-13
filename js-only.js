24b4c7cf70fdf6dc8559285c208163a5fb5fe759
Irakarama-Laurent123 <96382900+Irakarama-Laurent123@users.noreply.github.com>
2022-07-13 17:09:04 +0200

    simple addition

--- js-only.js
+++ js-only.js
@@ -5,6 +5,8 @@ const input2 = document.getElementById("num2");
 function add(num1, num2) {
   if(typeof num1 === 'number' && typeof num2 === 'number') {
   return num1 + num2;
+}else{
+  return +num1 + +num2;
 }
 }
 

bf15dfaf45cd6a5a6b7242b8da3e86a515fdce77
Irakarama-Laurent123 <96382900+Irakarama-Laurent123@users.noreply.github.com>
2022-07-13 17:06:52 +0200

    Solving our little bug using vanilla JS

--- js-only.js
+++ js-only.js
@@ -3,7 +3,9 @@ const input1 = document.getElementById("num1");
 const input2 = document.getElementById("num2");
 
 function add(num1, num2) {
+  if(typeof num1 === 'number' && typeof num2 === 'number') {
   return num1 + num2;
+}
 }
 
 button.addEventListener("click", function() {
